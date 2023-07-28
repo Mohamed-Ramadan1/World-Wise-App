31fac850ebd0eb2107d1b0c5fff3b6ac5cdcaf77
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-07-28 21:14:37 +0300

    Handeling the problem of doublicate countreys.

--- src/components/Counterys/CountryList.jsx
+++ src/components/Counterys/CountryList.jsx
@@ -8,10 +8,18 @@ const CountryList = ({ cities, isLoading }) => {
     return (
       <Message message="Adding Your first city by clicking on the city on the map" />
     );
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.map((el) => el.country).includes(city.country))
+      return [
+        ...arr,
+        { country: city.country, emoji: city.emoji, id: city.id },
+      ];
+    else return arr;
+  }, []);
   return (
     <ul className={styles.countryList}>
-      {cities.map((city) => (
-        <CountryItem country={city} key={city.id} />
+      {countries.map((countery) => (
+        <CountryItem country={countery} key={countery.id} />
       ))}
     </ul>
   );
