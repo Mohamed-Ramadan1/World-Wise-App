c550b8d823c3911d7579151fa6bd7baa3564a268
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-07-28 10:27:42 +0300

    creating the NavPage component.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
 import Product from "./pages/Product";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
+
 const App = () => {
   return (
     <div>
@@ -12,6 +15,8 @@ const App = () => {
           <Route index element={<Homepage />} />
           <Route path="product" element={<Product />} />
           <Route path="pricing" element={<Pricing />} />
+          <Route path="app" element={<AppLayout />} />
+          <Route path="login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,7 @@
+import styles from "./AppLayout.module.css";
+
+const AppLayout = () => {
+  return <div className={styles.app}>content </div>;
+};
+
+export default AppLayout;
