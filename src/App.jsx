52257404170cbc7d2f427b89cb060807edde406a
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-07-28 05:30:23 +0300

    Start creating pages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 
 const App = () => {
-  const x = 23;
-  const date = "date";
   return (
     <div>
-      <h1>{date}</h1>
+      <h1>Hello vite</h1>
     </div>
   );
 };
