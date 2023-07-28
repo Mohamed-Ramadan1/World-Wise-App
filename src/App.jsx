6e56e01f82bdf51bc77f51403011111c17251909
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-07-28 06:02:56 +0300

    Modifay some files and images.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Homepage from "./pages/Homepage";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
+import Product from "./pages/Product";
 const App = () => {
   return (
     <div>
       <h1>Hello vite</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Homepage />} />
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
